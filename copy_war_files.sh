62b9d625ab112bd772235e4b1d3386347965b83a
Paul Hammant <paul@hammant.org>
2013-07-31 07:32:22 -0500

    mods

--- copy_war_files.sh
+++ copy_war_files.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 rm -rf apache-tomcat-7.0.41/webapps/a
+rm -rf apache-tomcat-7.0.41/webapps/a.war
 rm -rf apache-tomcat-7.0.41/webapps/b
+rm -rf apache-tomcat-7.0.41/webapps/b.war
+rm -rf apache-tomcat-7.0.41/webapps/ROOT
+rm -rf apache-tomcat-7.0.41/webapps/ROOT.war
 cp a/target/a.war apache-tomcat-7.0.41/webapps/
-cp b/target/b.war apache-tomcat-7.0.41/webapps/
+cp b/target/b.war apache-tomcat-7.0.41/webapps/ROOT.war
